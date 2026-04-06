65209ab8a332a797e9267d3a1a2c720b113dcce7
Loki <lokeshirkhede10@gmail.com>
2026-04-07 01:17:26 +0530

    new merge

---
+++ main2.java
@@ -0,0 +1,8 @@
+public class main2{
+
+    public static void main2(String[] args) {
+        System.out.println("Hello World");
+
+        System.out.println("Hello World   22");
+             System.out.println("Hello World   33");
+    }}
